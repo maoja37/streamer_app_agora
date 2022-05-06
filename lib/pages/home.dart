1928d0b1d71bce1f783af428fb4e3609aea2ccc9
nuyiwa908 <maoja37@gmail.com>
2022-05-07 00:21:18 +0100

    needed to increase streaks

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -73,7 +73,7 @@ class _HomeState extends State<Home> {
                     Icon(Icons.cut),
                   ])),
         ],
-      )),                      
+      )),                                                                                                                               
     );
   }
 }
