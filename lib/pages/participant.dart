71843462402d2e45b295d27894b6ea08e5f0a86d
nuyiwa908 <maoja37@gmail.com>
2022-05-07 03:44:27 +0100

    added way more stuff

--- lib/pages/participant.dart
+++ lib/pages/participant.dart
@@ -2,13 +2,19 @@ import 'package:agora_rtc_engine/rtc_engine.dart';
 import 'package:agora_rtm/agora_rtm.dart';
 import 'package:flutter/material.dart';
 
+import 'package:agora_rtc_engine/rtc_local_view.dart' as RtcLocalView;
+import 'package:agora_rtc_engine/rtc_remote_view.dart' as RtcRemoteView;
 import '../utils/appId.dart';
 
 class Participant extends StatefulWidget {
   final String channelName;
   final String userName;
+  final int uid;
   const Participant(
-      {Key? key, required this.channelName, required this.userName})
+      {Key? key,
+      required this.uid,
+      required this.channelName,
+      required this.userName})
       : super(key: key);
 
   @override
@@ -20,7 +26,10 @@ class _ParticipantState extends State<Participant> {
   late RtcEngine _engine;
   AgoraRtmClient? _client;
   AgoraRtmChannel? _channel;
+  bool muted = false;
+  bool videoDisabled = false;
 
+  
   @override
   void initState() {
     super.initState();
@@ -37,6 +46,7 @@ class _ParticipantState extends State<Participant> {
     //Callbacks for the RTC Engine
     _engine.setEventHandler(
         RtcEngineEventHandler(joinChannelSuccess: (channel, uid, elapsed) {
+      //TODO: Add join channel logic
       setState(() {
         _users.add(uid);
       });
@@ -48,30 +58,57 @@ class _ParticipantState extends State<Participant> {
     };
 
     _client?.onConnectionStateChanged = (int state, int reason) {
-      print('Connection State Changed: ' + state.toString()+ ', reason: ' + reason.toString());
-      if(state == 5){
+      print('Connection State Changed: ' +
+          state.toString() +
+          ', reason: ' +
+          reason.toString());
+      if (state == 5) {
         _channel?.leave();
         _client?.logout();
         _client?.destroy();
         print('Logged out');
-         
       }
     };
 
+    // Join the RTM and RTC channels
+    await _client?.login(null, widget.uid.toString());
+    _channel = await _client?.createChannel(widget.channelName);
+    await _channel?.join();
+    await _engine.joinChannel(null, widget.channelName, null, widget.uid);
 
     //Callbacks for the RTM Channel
     _channel?.onMemberJoined = (AgoraRtmMember member) {
-      print("Member joined: " + member.userId + ', channel: ' + member.channelId);
+      print(
+          "Member joined: " + member.userId + ', channel: ' + member.channelId);
     };
     _channel?.onMemberLeft = (AgoraRtmMember member) {
       print("Member left: " + member.userId + ', channel: ' + member.channelId);
+    };
+
+    _channel?.onMessageReceived =
+        (AgoraRtmMessage message, AgoraRtmMember member) {
+      //TODO: implement this later
     };
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Participant')),
+    return Scaffold(
+      body: Center(
+          child: Stack(
+        children: [
+          _broadcastView(),
+          _toolbar(),
+        ],
+      )),
     );
+  }
+
+  Widget _toolbar() {
+    return Container();
+  }
+
+  Widget _broadcastView() {
+    return Expanded(child: RtcLocalView.SurfaceView());
   }
 }
