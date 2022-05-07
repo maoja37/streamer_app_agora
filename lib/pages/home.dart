8f4473ea12d107cde89566fbbd3dd87f0da1238b
nuyiwa908 <maoja37@gmail.com>
2022-05-07 03:26:17 +0100

    added more stuff

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:streamer/pages/director.dart';
 import 'package:streamer/pages/participant.dart';
 
@@ -13,6 +14,31 @@ class _HomeState extends State<Home> {
 
   final _channelName = TextEditingController();
   final _userName = TextEditingController();
+  late int uid;
+
+  @override
+  void initState() {
+    getUserId();
+    super.initState();
+  }
+
+  Future<void> getUserId() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    int? storedUid = preferences.getInt('localUid');
+     if (storedUid != null) {
+      uid = storedUid;
+      print("storedUID: $uid");
+    } else {
+      //should only happen once, unless they delete the app
+      int time = DateTime.now().millisecondsSinceEpoch;
+      uid = int.parse(time.toString().substring(1, time.toString().length - 3));
+      preferences.setInt("localUid", uid);
+      print("settingUID: $uid");
+    }
+                                                     
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(         
@@ -53,7 +79,7 @@ class _HomeState extends State<Home> {
           TextButton(               
               onPressed: () {
                 Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) =>  Participant(channelName: _channelName.text,  userName: _userName.text,)));
+                    MaterialPageRoute(builder: (context) =>  Participant(channelName: _channelName.text,  userName: _userName.text, uid: uid,)));
               },
               child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -70,7 +96,7 @@ class _HomeState extends State<Home> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: const [
                     Text('Director', style: TextStyle(fontSize: 20)),
-                    Icon(Icons.cut),
+                    Icon(Icons.cut), 
                   ])),
         ],
       )),                                                                                                                               
